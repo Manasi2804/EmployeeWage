1087d757a74dc32f06c5eba6121876d392ca963e
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-01 13:39:29 +0530

    Added: Add part time employee using case statement

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -12,12 +12,17 @@ public class EmpWage {
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
+        }
         // calculate daily employee wage
         empWage = empHrs * EMP_RATE_PER_HOURS;
         System.out.println("Daily Employee Wage:" + empWage);
