4f2a78f3c2e726fbcee7c1ae928c46347775f359
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-01 14:20:05 +0530

    Added: Calculate wage for month

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -5,32 +5,37 @@ public class EmpWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOURS = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
-        // checking employee is present or absent
+    // checking employee is present or absent
 
-    public static void calculateDailyEmpWage() {
+    public static void calculateWageForMonth() {
         int empHrs = 0;
         int empWage = 0;
+        int day;
+        double salary, totalsalary=0;
 
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        for(day=1; day<=NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+            }
+            // calculate daily employee wage
+            salary = empHrs * EMP_RATE_PER_HOURS;
+            totalsalary = totalsalary + salary;
         }
-        // calculate daily employee wage
-        empWage = empHrs * EMP_RATE_PER_HOURS;
-        System.out.println("Daily Employee Wage:" + empWage);
+        System.out.println("Monthly Employee Wage:" + totalsalary);
     }
-
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage");
-        calculateDailyEmpWage();
+        calculateWageForMonth();
     }
 }
