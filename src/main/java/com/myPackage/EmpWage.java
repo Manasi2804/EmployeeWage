5a752e0d801f8204b88f6374c848ce1c0744c6cb
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-01 18:26:00 +0530

    Added: Calculate the wages till condition

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -1,21 +1,21 @@
 package com.myPackage;
 
 public class EmpWage {
-
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOURS = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int NUM_OF_HOURS = 100;
 
     // checking employee is present or absent
 
     public static void calculateWageForMonth() {
         int empHrs = 0;
-        int empWage = 0;
-        int day;
-        double salary, totalsalary=0;
-
-        for(day=1; day<=NUM_OF_WORKING_DAYS; day++) {
+        int totalEmpHr = 0;
+        int day=0;
+        double salary, totalSalary = 0;
+        while (totalEmpHr<= NUM_OF_HOURS && day < NUM_OF_WORKING_DAYS) {
+            day++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
                 case IS_FULL_TIME:
@@ -27,14 +27,14 @@ public class EmpWage {
                 default:
                     empHrs = 0;
             }
-            // calculate daily employee wage
-            salary = empHrs * EMP_RATE_PER_HOURS;
-            totalsalary = totalsalary + salary;
+            totalSalary = totalEmpHr + empHrs;
         }
-        System.out.println("Monthly Employee Wage:" + totalsalary);
+        // calculate daily employee wage
+        totalSalary = totalEmpHr * EMP_RATE_PER_HOURS;
+        System.out.println("Monthly Employee Wage:" + totalSalary);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]args){
         System.out.println("Welcome to employee wage");
         calculateWageForMonth();
     }
