c94ad9e8c9c818dc704a487d81f175ac26e2f7e4
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-01 12:12:05 +0530

    Added: Calculate daily wage

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -2,13 +2,24 @@ package com.myPackage;
 
 public class EmpWage {
 
-    public static void main(String[] args) {
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HOURS = 20;
 
-        int isPresent = 1;
+        // checking employee is present or absent
+
+    public static void calculateDailyEmpWage() {
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == isPresent)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+
+        // calculate daily employee wage
+        empWage=empHrs * EMP_RATE_PER_HOURS;
+        System.out.println("Daily Employee Wage:" + empWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage");
     }
 }
