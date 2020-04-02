6461a4dc8fd86492600447ed90c6d6c13871e164
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-02 10:52:20 +0530

    Added: Store daily wage

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -27,6 +27,7 @@ public class EmpWage {
             int empHr = 0;
             int totalEmpHr = 0;
             int totalWorkingDays = 0;
+            int totalMonthlywage=0;
             double salary, totalSalary = 0;
             while (totalWorkingDays < MAX_DAYS && totalEmpHr < MAX_WORKING_HOUR)
             {
@@ -35,14 +36,26 @@ public class EmpWage {
                 empHr = getWorkingHours(choice);
                 // calculate total emp hours
                 totalEmpHr = (totalEmpHr + empHr);
-                // total salary
+                // calculate daily emp hour for month
+                int array[]=new int [25];
                 totalWorkingDays++;
+                array[totalWorkingDays]=dailyWage(empHr);
+                System.out.println("array[totalWorkingDays]");
+
+                // monthly wage
+                totalMonthlywage=(totalMonthlywage+array[totalWorkingDays]);
             }
+            System.out.println(totalMonthlywage);
             totalSalary = (totalEmpHr + WAGE_PER_HOUR);
             System.out.println("total salary" + totalSalary);
         }
-
-
+        // create method
+        public int dailyWage(int empHr)
+        {
+            int dailywage=0;
+            dailywage=(WAGE_PER_HOUR*empHr);
+            return dailywage;
+        }
     public static void main(String[]args) {
         // object of class
         EmpWage emp = new EmpWage();
