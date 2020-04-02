7e258ddad99776e42269dddc493031afba6f709c
Manasi2804 <shahmanasi281996@gmail.com>
2020-04-02 10:24:13 +0530

    Added:A function to get working hours of an employee

--- src/main/java/com/myPackage/EmpWage.java
+++ src/main/java/com/myPackage/EmpWage.java
@@ -2,40 +2,52 @@ package com.myPackage;
 
 public class EmpWage {
     public static final int IS_FULL_TIME = 1;
-    public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOURS = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int NUM_OF_HOURS = 100;
+    public static final int IS_PART_TIME =2;
+    public static final int MAX_WORKING_HOUR=100;
+    public static final int MAX_DAYS=20;
+    public static final int WAGE_PER_HOUR=20;
 
-    // checking employee is present or absent
-
-    public static void calculateWageForMonth() {
+    public int getWorkingHours(int choice) {
         int empHrs = 0;
-        int totalEmpHr = 0;
-        int day=0;
-        double salary, totalSalary = 0;
-        while (totalEmpHr<= NUM_OF_HOURS && day < NUM_OF_WORKING_DAYS) {
-            day++;
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-            }
-            totalSalary = totalEmpHr + empHrs;
+        switch (choice) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
-        // calculate daily employee wage
-        totalSalary = totalEmpHr * EMP_RATE_PER_HOURS;
-        System.out.println("Monthly Employee Wage:" + totalSalary);
+        return empHrs;
+    }
+    //method create
+        public void calculateEmployeeHours()
+        {
+            int empHr = 0;
+            int totalEmpHr = 0;
+            int totalWorkingDays = 0;
+            double salary, totalSalary = 0;
+            while (totalWorkingDays < MAX_DAYS && totalEmpHr < MAX_WORKING_HOUR)
+            {
+                int choice= (int) Math.floor(Math.random() * 10) % 3;
+                //method call
+                empHr = getWorkingHours(choice);
+                // calculate total emp hours
+                totalEmpHr = (totalEmpHr + empHr);
+                // total salary
+                totalWorkingDays++;
+            }
+            totalSalary = (totalEmpHr + WAGE_PER_HOUR);
+            System.out.println("total salary" + totalSalary);
+        }
+
+
+    public static void main(String[]args) {
+        // object of class
+        EmpWage emp = new EmpWage();
+        // call method
+        emp.calculateEmployeeHours();
+    }
     }
 
-    public static void main(String[]args){
-        System.out.println("Welcome to employee wage");
-        calculateWageForMonth();
-    }
-}
